b0c195e9df124db7bc1dfbbac53edaa8ae8bab82
yuuki yamaguchi <yuukiyamaguchi5@gmail.com>
2022-06-11 11:20:59 +0900

    create electron template

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    resizable: false
+  });
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
